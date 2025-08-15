a6ec7b832338d7e9c3462e14754cec52336d9b7d
Mehran Bakhtiari <mehbakh82@Mehrans-MacBook-Pro.local>
2025-08-15 19:46:06 +0330

    Add TestRunner class

---
+++ src/test/java/com/example/calculator/TestRunner.java
@@ -0,0 +1,10 @@
+package com.example.calculator;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources", glue = "com.example.calculator")
+public class TestRunner {
+}
